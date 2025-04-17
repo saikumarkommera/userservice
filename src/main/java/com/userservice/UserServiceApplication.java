f853337f82fcc1b6387aa929702a5eef111fc8c2
sai <laddusai520@gmail.com>
2025-04-17 15:19:47 +0530

    Updated Logs....

--- src/main/java/com/userservice/UserServiceApplication.java
+++ src/main/java/com/userservice/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+//Main class
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
