117598c86af4e957aa198a3c8720a6a11e2f128f
sai <laddusai520@gmail.com>
2025-05-02 09:29:25 +0530

    Added method docs

--- src/main/java/com/userservice/exception/ProductException.java
+++ src/main/java/com/userservice/exception/ProductException.java
@@ -1,5 +1,8 @@
 package com.userservice.exception;
 
+/**
+ * Exception classs
+ */
 public class ProductException extends RuntimeException{
     public ProductException(String message){
         super(message);
