9816793bbae9aef165fd630b31002f750f2fde31
sai <laddusai520@gmail.com>
2025-05-05 11:38:15 +0530

    Removed docs

--- src/main/java/com/userservice/repository/OrderRepository.java
+++ src/main/java/com/userservice/repository/OrderRepository.java
@@ -3,10 +3,7 @@ package com.userservice.repository;
 import com.userservice.user.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-/**
- * Repository to connect to databases
- */
+s
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 }
