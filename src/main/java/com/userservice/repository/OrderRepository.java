7f99c73bf05d9290997c18d8790fcd21cb50efae
sai <laddusai520@gmail.com>
2025-05-05 11:33:35 +0530

    Updated method docs method docs

--- src/main/java/com/userservice/repository/OrderRepository.java
+++ src/main/java/com/userservice/repository/OrderRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Repository to connrct with databases
+ * Repository to connect to databases
  */
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
